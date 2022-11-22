046398ad58c27240dd2d71cfab199ec3d5361a18
MACARENASTORE <oclusiva9204@gmail.com>
2022-11-22 13:11:53 -0500

    first commit5

---
+++ src/componentes/router/router.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Login from "../login/login";
+import Inicio from "../index/index";
+
+export default function AppRoutes() {
+  return (
+    <Router>
+      <Switch>
+        <Route exact path={["/"]} component={Inicio} />
+        <Route exact path={["/login"]} component={Login} />
+        <Route exact path={["/index"]} component={Inicio} />
+
+        <Route
+          path={"*"}
+          component={() => (
+            <h1 style={{ marginTop: 300 }}>
+              404
+              <br />
+              Página no encontrada
+            </h1>
+          )}
+        />
+      </Switch>
+    </Router>
+  );
+}
